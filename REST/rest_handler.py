ca6dc416b5d189bb5e7645e592370a17fdcba1fc
Alexander Falk <alef@netcompany.com>
2020-02-13 22:38:01 +0100

    Added anonymization path and file upload

--- REST/rest_handler.py
+++ REST/rest_handler.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser(description="With this tool, you can start an H
 
 parser.add_argument('--host', action='store', type=str, help='The hostname / address the server has')
 parser.add_argument('--port', action='store', type=int, help='The port for the server to listen on')
-
+parser.add_argument('--lang', action='store', type=str, help='The language of the model you want to use. Default is Danish')
+parser.add_argument('--hardware', action='store', type=str, help='Whether to use GPU or CPU')
 args = parser.parse_args()
 
 server = Server(host=args.host, port=args.port)
